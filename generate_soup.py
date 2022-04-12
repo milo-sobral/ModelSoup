5b3a4627dbfa2b0ee9f99fa8a4dc2a3085d0d1dc
charlo1998 <charles19982009@hotmail.com>
2022-04-12 11:01:07 -0400

    added pruned soups

--- generate_soup.py
+++ generate_soup.py
@@ -6,12 +6,18 @@ import argparse
 import torch
 import os
 
-METHODS = ['UNIFORM', 'GREEDY']
+METHODS = ['UNIFORM', 'GREEDY', 'PRUNED']
 
 def avg_weights(old_weights, new_weights, N):
     for idx, weight_matrix in enumerate(old_weights):
         old_weights[idx] = (N * weight_matrix) + new_weights[idx] / (N + 1)
     return old_weights, N+1
+
+def remove_weights(old_weights, weight_removed, N):
+    for idx, weight_matrix in enumerate(old_weights):
+        old_weights[idx] = (N * weight_matrix) - new_weights[idx] / (N - 1)
+    return old_weights, N-1
+
 
 def make_soup(models_folder, method, model_class, initial_model_file, eval_func=None):
 
@@ -35,7 +41,7 @@ def make_soup(models_folder, method, model_class, initial_model_file, eval_func=
         weights = [param for param in model_class.parameters()]
 
         # Create the soup
-        if method == "UNIFORM":
+        if method == "UNIFORM" or method == "PRUNED":
             final_weights, N = avg_weights(final_weights, weights, N)
         elif method == "GREEDY":
             new_performance = eval_func(model_class)
@@ -43,6 +49,26 @@ def make_soup(models_folder, method, model_class, initial_model_file, eval_func=
             if new_performance >= baseline_performance:
                 final_weights, N = avg_weights(final_weights, weights, N)
             baseline_performance = new_performance
+     
+    if method == "PRUNED":
+        baseline_performance = eval_func(final_weights)
+
+        for file in os.listdir(models_folder):
+            # TODO: Check that we have a model file and not something else
+
+            # Load model weights into the model class
+            model_class.load_state_dict(torch.load(file))
+
+            #prune the soup
+            final_weights, N = remove_weights(final_weights)
+            new_performance = eval_func(final_weights)
+
+            #if the performance drops, add back to the soup
+            if new_performance < baseline_performance:
+                final_weights, N = avg_weights(final_weights, weights, N)
+            else:
+                baseline_performance = new_performance
+
                 
 
 if __name__ == "__main__":
