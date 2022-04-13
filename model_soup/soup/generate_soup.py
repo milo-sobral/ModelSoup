7e57fd2b2f579738c1bf5d75457368f95c566d57
Milo Sobral <35710239+milo-sobral@users.noreply.github.com>
2022-04-13 18:53:19 -0400

    Added comments and some small fixes

--- model_soup/soup/generate_soup.py
+++ model_soup/soup/generate_soup.py
@@ -22,7 +22,24 @@ def add_ingradient(soup, path, N):
     return soup, N+1
 
 
-def make_soup(models_folder, soup, evaluator, device, method=Methods.GREEDY, initial_model_file=None):
+def make_soup(models_folder, soup, evaluator, device=None, method=Methods.GREEDY, initial_model_file=None):
+    '''
+    Generates a soup using the given evaluator and method, returns soup and best performance
+    Inputs:
+        - models_folder : [required] folder containing pytorch trained models to use
+        - soup : [required] Pytorch Class of the desired model
+        - evaluator : [required] An evaluator object which will be used to evaluate the performance of the model
+        - device : Torch device to run inference
+        - method : One of Greedy or Uniform
+        - initial_model_file : specify which file to use initially
+    Returns: 
+        - soup : The soup made according to the method specified
+        - final_performance : The final performance according to the specified evaluator of the model
+        - N : Number of ingradients used in the final soup
+    '''
+
+    if device is None:
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     all_model_files = os.listdir(models_folder)
 
@@ -35,7 +52,6 @@ def make_soup(models_folder, soup, evaluator, device, method=Methods.GREEDY, ini
     soup.to(device)
     soup.eval()
 
-    # final_weights = [param.data for param in soup.parameters()]
     N = 1
 
     if method == Methods.GREEDY:
@@ -55,11 +71,13 @@ def make_soup(models_folder, soup, evaluator, device, method=Methods.GREEDY, ini
                 if new_performance >= baseline_performance:
                     soup = soup_next
                     baseline_performance = new_performance
+                else:
+                    N -= 1
             elif method == Methods.UNIFORM:
                 soup = soup_next
             else:
                 raise NotImplemented                     
 
     final_performance = evaluator.eval_func(soup)
-    return soup, final_performance
+    return soup, final_performance, N
 
