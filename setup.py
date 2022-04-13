33158b4379bd5a93e0a6ca9f792fbf4ad6be1713
Milo Sobral <35710239+milo-sobral@users.noreply.github.com>
2022-04-13 10:41:28 -0400

    Fixed bug with setup.py script

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ setup(
    name='model_soup',
    version='1.0',
    description='Package to test model soups with Resnets and the CIFAR-100 Dataset',
-   packages=['foo'],  #same as name
+   packages=['model_soup'],  #same as name
    url='https://github.com/milo-sobral/ModelSoup',
    install_requires=[
        'torch', 
        'torchvision', 
-       'argparse',
-       'os'], #external packages as dependencies
+       'argparse'], #external packages as dependencies
 )
